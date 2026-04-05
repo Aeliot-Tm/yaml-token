ddd8a8d7c7919a599227056617a601632dca79a3
Anatoliy Melnikov <5785276@gmail.com>
2026-04-05 18:02:07 +0300

    Implement lexer mapping test for spec 1.2.1

---
+++ tests/unit/Lexer/Spec/V1P2P1/LexerMappingSingleQuotedTest.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the YAML Token project.
+ *
+ * (c) Anatoliy Melnikov <5785276@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Aeliot\YamlToken\Test\Unit\Lexer\Spec\V1P2P1;
+
+use Aeliot\YamlToken\Enum\TokenType;
+use Aeliot\YamlToken\Lexer\Lexer;
+use Aeliot\YamlToken\Test\Unit\Lexer\LexerMappingTestCase;
+use Aeliot\YamlToken\Token\Token;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\UsesClass;
+
+#[CoversClass(Lexer::class)]
+#[UsesClass(Token::class)]
+#[UsesClass(TokenType::class)]
+final class LexerMappingSingleQuotedTest extends LexerMappingTestCase
+{
+    public static function getDataForTestMapping(): iterable
+    {
+        yield [[
+            [
+                'type' => TokenType::PLAIN_SCALAR,
+                'text' => 'key',
+            ],
+            [
+                'type' => TokenType::MAPPING_VALUE,
+                'text' => ':',
+            ],
+            [
+                'type' => TokenType::WHITESPACE,
+                'text' => ' ',
+            ],
+            [
+                'type' => TokenType::SINGLE_QUOTED_SCALAR,
+                'text' => '\'single quoted value\'',
+            ],
+            [
+                'type' => TokenType::NEWLINE,
+                'text' => '
+',
+            ],
+        ], __DIR__.'/../../../../fixture/spec/1.2.1/single-quoted.yaml'];
+    }
+}
