3efc3bdf0679794d8604213b3b585b703984a81a
Anatoliy Melnikov <5785276@gmail.com>
2026-04-05 17:58:57 +0300

    Implement lexer mapping test for spec 1.1

---
+++ tests/unit/Lexer/Spec/V1P1/LexerMappingFoldedBlockTest.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the YAML Token project.
+ *
+ * (c) Anatoliy Melnikov <5785276@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Aeliot\YamlToken\Test\Unit\Lexer\Spec\V1P1;
+
+use Aeliot\YamlToken\Enum\TokenType;
+use Aeliot\YamlToken\Lexer\Lexer;
+use Aeliot\YamlToken\Test\Unit\Lexer\LexerMappingTestCase;
+use Aeliot\YamlToken\Token\Token;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\UsesClass;
+
+#[CoversClass(Lexer::class)]
+#[UsesClass(Token::class)]
+#[UsesClass(TokenType::class)]
+final class LexerMappingFoldedBlockTest extends LexerMappingTestCase
+{
+    public static function getDataForTestMapping(): iterable
+    {
+        yield [[
+            [
+                'type' => TokenType::PLAIN_SCALAR,
+                'text' => 'key',
+            ],
+            [
+                'type' => TokenType::MAPPING_VALUE,
+                'text' => ':',
+            ],
+            [
+                'type' => TokenType::WHITESPACE,
+                'text' => ' ',
+            ],
+            [
+                'type' => TokenType::FOLDED_BLOCK_SCALAR,
+                'text' => '>
+  This is a long line
+  that folds into space.
+
+  New paragraph here.
+',
+            ],
+        ], __DIR__.'/../../../../fixture/spec/1.1/folded-block.yaml'];
+    }
+}
---
+++ tests/unit/Lexer/Spec/V1P1/LexerMappingTagOmapTest.php
@@ -0,0 +1,129 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the YAML Token project.
+ *
+ * (c) Anatoliy Melnikov <5785276@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Aeliot\YamlToken\Test\Unit\Lexer\Spec\V1P1;
+
+use Aeliot\YamlToken\Enum\TokenType;
+use Aeliot\YamlToken\Lexer\Lexer;
+use Aeliot\YamlToken\Test\Unit\Lexer\LexerMappingTestCase;
+use Aeliot\YamlToken\Token\Token;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\UsesClass;
+
+#[CoversClass(Lexer::class)]
+#[UsesClass(Token::class)]
+#[UsesClass(TokenType::class)]
+final class LexerMappingTagOmapTest extends LexerMappingTestCase
+{
+    public static function getDataForTestMapping(): iterable
+    {
+        yield [[
+            [
+                'type' => TokenType::TAG,
+                'text' => '!!omap',
+            ],
+            [
+                'type' => TokenType::NEWLINE,
+                'text' => '
+',
+            ],
+            [
+                'type' => TokenType::SEQUENCE_ENTRY,
+                'text' => '-',
+            ],
+            [
+                'type' => TokenType::WHITESPACE,
+                'text' => ' ',
+            ],
+            [
+                'type' => TokenType::PLAIN_SCALAR,
+                'text' => 'Mark McGwire',
+            ],
+            [
+                'type' => TokenType::MAPPING_VALUE,
+                'text' => ':',
+            ],
+            [
+                'type' => TokenType::WHITESPACE,
+                'text' => ' ',
+            ],
+            [
+                'type' => TokenType::PLAIN_SCALAR,
+                'text' => '65',
+            ],
+            [
+                'type' => TokenType::NEWLINE,
+                'text' => '
+',
+            ],
+            [
+                'type' => TokenType::SEQUENCE_ENTRY,
+                'text' => '-',
+            ],
+            [
+                'type' => TokenType::WHITESPACE,
+                'text' => ' ',
+            ],
+            [
+                'type' => TokenType::PLAIN_SCALAR,
+                'text' => 'Sammy Sosa',
+            ],
+            [
+                'type' => TokenType::MAPPING_VALUE,
+                'text' => ':',
+            ],
+            [
+                'type' => TokenType::WHITESPACE,
+                'text' => ' ',
+            ],
+            [
+                'type' => TokenType::PLAIN_SCALAR,
+                'text' => '63',
+            ],
+            [
+                'type' => TokenType::NEWLINE,
+                'text' => '
+',
+            ],
+            [
+                'type' => TokenType::SEQUENCE_ENTRY,
+                'text' => '-',
+            ],
+            [
+                'type' => TokenType::WHITESPACE,
+                'text' => ' ',
+            ],
+            [
+                'type' => TokenType::PLAIN_SCALAR,
+                'text' => 'Ken Griffey',
+            ],
+            [
+                'type' => TokenType::MAPPING_VALUE,
+                'text' => ':',
+            ],
+            [
+                'type' => TokenType::WHITESPACE,
+                'text' => ' ',
+            ],
+            [
+                'type' => TokenType::PLAIN_SCALAR,
+                'text' => '58',
+            ],
+            [
+                'type' => TokenType::NEWLINE,
+                'text' => '
+',
+            ],
+        ], __DIR__.'/../../../../fixture/spec/1.1/tag-omap.yaml'];
+    }
+}
