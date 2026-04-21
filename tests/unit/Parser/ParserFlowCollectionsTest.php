46bbed77dd020be8a56456c2f5371d1b61f41f5b
Anatoliy Melnikov <5785276@gmail.com>
2026-04-21 19:51:01 +0300

    Implement tests covering parser

---
+++ tests/unit/Parser/ParserFlowCollectionsTest.php
@@ -0,0 +1,124 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the YAML Token project.
+ *
+ * (c) Anatoliy Melnikov <5785276@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Aeliot\YamlToken\Test\Unit\Parser;
+
+use Aeliot\YamlToken\Lexer\Lexer;
+use Aeliot\YamlToken\Node\DocumentNode;
+use Aeliot\YamlToken\Node\FlowMappingNode;
+use Aeliot\YamlToken\Node\FlowSequenceNode;
+use Aeliot\YamlToken\Node\KeyValueCoupleNode;
+use Aeliot\YamlToken\Node\ScalarNode;
+use Aeliot\YamlToken\Node\StreamNode;
+use Aeliot\YamlToken\Node\ValueNode;
+use Aeliot\YamlToken\Parser\Parser;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\UsesClass;
+use PHPUnit\Framework\TestCase;
+
+#[CoversClass(Parser::class)]
+#[UsesClass(DocumentNode::class)]
+#[UsesClass(FlowMappingNode::class)]
+#[UsesClass(FlowSequenceNode::class)]
+#[UsesClass(KeyValueCoupleNode::class)]
+#[UsesClass(Lexer::class)]
+#[UsesClass(ScalarNode::class)]
+#[UsesClass(StreamNode::class)]
+#[UsesClass(ValueNode::class)]
+final class ParserFlowCollectionsTest extends TestCase
+{
+    public function testParsesFlowSequenceAsValue(): void
+    {
+        $stream = (new Parser())->parse(<<<'YAML'
+key: [one, two]
+YAML);
+
+        $couple = $this->getOnlyCouple($stream);
+        $value = $couple->getValue();
+        self::assertInstanceOf(ValueNode::class, $value);
+
+        $flows = array_values(array_filter(
+            $value->getChildren(),
+            static fn ($n): bool => $n instanceof FlowSequenceNode,
+        ));
+        self::assertCount(1, $flows);
+
+        $entries = array_values(array_filter(
+            $flows[0]->getEntries(),
+            static fn ($n): bool => $n instanceof ValueNode,
+        ));
+        self::assertCount(2, $entries);
+        self::assertSame('one', $entries[0]->getScalar()?->getToken()->text);
+        self::assertSame('two', $entries[1]->getScalar()?->getToken()->text);
+    }
+
+    public function testParsesFlowMappingAsValue(): void
+    {
+        $stream = (new Parser())->parse(<<<'YAML'
+key: {a: 1, b: 2}
+YAML);
+
+        $couple = $this->getOnlyCouple($stream);
+        $value = $couple->getValue();
+        self::assertInstanceOf(ValueNode::class, $value);
+
+        $flows = array_values(array_filter(
+            $value->getChildren(),
+            static fn ($n): bool => $n instanceof FlowMappingNode,
+        ));
+        self::assertCount(1, $flows);
+
+        $flowMapping = $flows[0];
+        $flowCouples = array_values(array_filter(
+            $flowMapping->getChildren(),
+            static fn ($n): bool => $n instanceof KeyValueCoupleNode,
+        ));
+
+        self::assertCount(2, $flowCouples);
+        self::assertSame(['a', 'b'], array_map(fn (KeyValueCoupleNode $c): string => $this->getKeyText($c), $flowCouples));
+        self::assertSame('1', $this->getScalarValueText($flowCouples[0]));
+        self::assertSame('2', $this->getScalarValueText($flowCouples[1]));
+    }
+
+    private function getKeyText(KeyValueCoupleNode $couple): string
+    {
+        return $couple->getKey()->getName()->getToken()->text;
+    }
+
+    private function getOnlyCouple(StreamNode $stream): KeyValueCoupleNode
+    {
+        $documents = array_values(array_filter(
+            $stream->getChildren(),
+            static fn ($n): bool => $n instanceof DocumentNode,
+        ));
+        self::assertCount(1, $documents);
+
+        $couples = array_values(array_filter(
+            $documents[0]->getChildren(),
+            static fn ($n): bool => $n instanceof KeyValueCoupleNode,
+        ));
+        self::assertCount(1, $couples);
+
+        return $couples[0];
+    }
+
+    private function getScalarValueText(KeyValueCoupleNode $couple): string
+    {
+        $valueNode = $couple->getValue();
+        self::assertInstanceOf(ValueNode::class, $valueNode);
+        $scalar = $valueNode->getScalar();
+        self::assertInstanceOf(ScalarNode::class, $scalar);
+
+        return $scalar->getToken()->text;
+    }
+}
---
+++ tests/unit/Parser/ParserUnsupportedCasesTest.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the YAML Token project.
+ *
+ * (c) Anatoliy Melnikov <5785276@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Aeliot\YamlToken\Test\Unit\Parser;
+
+use Aeliot\YamlToken\Parser\Exception\UnexpectedStateException;
+use Aeliot\YamlToken\Parser\Exception\UnexpectedTokenException;
+use Aeliot\YamlToken\Parser\Parser;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+
+#[CoversClass(Parser::class)]
+final class ParserUnsupportedCasesTest extends TestCase
+{
+    public function testThrowsWhenBlockMappingValueContainsNoCouples(): void
+    {
+        $this->expectException(UnexpectedTokenException::class);
+        $this->expectExceptionMessage('Key/value couple expected while parsing block mapping value');
+
+        (new Parser())->parse(<<<'YAML'
+a:
+  # only comments, no key/value
+YAML);
+    }
+
+    public function testThrowsWhenMergeValueIsNotAliasOrFlowSequenceOfAliases(): void
+    {
+        $this->expectException(UnexpectedStateException::class);
+        $this->expectExceptionMessage('Merge value must be an alias or a flow sequence of aliases');
+
+        (new Parser())->parse(<<<'YAML'
+a:
+  <<: not-an-alias
+YAML);
+    }
+}
