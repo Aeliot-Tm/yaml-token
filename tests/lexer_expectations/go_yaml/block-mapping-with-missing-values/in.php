a840bc1fd46f2eeb83f4162f59622bd1ba48e671
Anatoliy Melnikov <5785276@gmail.com>
2026-05-02 16:14:12 +0300

    Covering testing

---
+++ tests/lexer_expectations/go_yaml/block-mapping-with-missing-values/in.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+use Aeliot\YamlToken\Enum\TokenType;
+
+return [
+    [
+        'type' => TokenType::EXPLICIT_KEY_INDICATOR,
+        'text' => '?',
+    ],
+    [
+        'type' => TokenType::WHITESPACE,
+        'text' => ' ',
+    ],
+    [
+        'type' => TokenType::PLAIN_SCALAR,
+        'text' => 'a',
+    ],
+    [
+        'type' => TokenType::NEWLINE,
+        'text' => "\n",
+    ],
+    [
+        'type' => TokenType::EXPLICIT_KEY_INDICATOR,
+        'text' => '?',
+    ],
+    [
+        'type' => TokenType::WHITESPACE,
+        'text' => ' ',
+    ],
+    [
+        'type' => TokenType::PLAIN_SCALAR,
+        'text' => 'b',
+    ],
+    [
+        'type' => TokenType::NEWLINE,
+        'text' => "\n",
+    ],
+    [
+        'type' => TokenType::PLAIN_SCALAR,
+        'text' => 'c',
+    ],
+    [
+        'type' => TokenType::VALUE_INDICATOR,
+        'text' => ':',
+    ],
+    [
+        'type' => TokenType::NEWLINE,
+        'text' => "\n",
+    ],
+];
