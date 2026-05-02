ac273c167393c456b94c5e5d34ee73d44bec3c9d
Anatoliy Melnikov <5785276@gmail.com>
2026-05-02 20:39:09 +0300

    Covering testing

---
+++ tests/lexer_expectations/go_yaml/block-mappings-in-block-sequence/in.php
@@ -0,0 +1,100 @@
+<?php
+
+declare(strict_types=1);
+
+use Aeliot\YamlToken\Enum\TokenType;
+
+return [
+    [
+        'type' => TokenType::INDENTATION,
+        'text' => ' ',
+    ],
+    [
+        'type' => TokenType::SEQUENCE_ENTRY,
+        'text' => '-',
+    ],
+    [
+        'type' => TokenType::WHITESPACE,
+        'text' => ' ',
+    ],
+    [
+        'type' => TokenType::PLAIN_SCALAR,
+        'text' => 'key',
+    ],
+    [
+        'type' => TokenType::VALUE_INDICATOR,
+        'text' => ':',
+    ],
+    [
+        'type' => TokenType::WHITESPACE,
+        'text' => ' ',
+    ],
+    [
+        'type' => TokenType::PLAIN_SCALAR,
+        'text' => 'value',
+    ],
+    [
+        'type' => TokenType::NEWLINE,
+        'text' => "\n",
+    ],
+    [
+        'type' => TokenType::INDENTATION,
+        'text' => '   ',
+    ],
+    [
+        'type' => TokenType::PLAIN_SCALAR,
+        'text' => 'key2',
+    ],
+    [
+        'type' => TokenType::VALUE_INDICATOR,
+        'text' => ':',
+    ],
+    [
+        'type' => TokenType::WHITESPACE,
+        'text' => ' ',
+    ],
+    [
+        'type' => TokenType::PLAIN_SCALAR,
+        'text' => 'value2',
+    ],
+    [
+        'type' => TokenType::NEWLINE,
+        'text' => "\n",
+    ],
+    [
+        'type' => TokenType::INDENTATION,
+        'text' => ' ',
+    ],
+    [
+        'type' => TokenType::SEQUENCE_ENTRY,
+        'text' => '-',
+    ],
+    [
+        'type' => TokenType::NEWLINE,
+        'text' => "\n",
+    ],
+    [
+        'type' => TokenType::INDENTATION,
+        'text' => '   ',
+    ],
+    [
+        'type' => TokenType::PLAIN_SCALAR,
+        'text' => 'key3',
+    ],
+    [
+        'type' => TokenType::VALUE_INDICATOR,
+        'text' => ':',
+    ],
+    [
+        'type' => TokenType::WHITESPACE,
+        'text' => ' ',
+    ],
+    [
+        'type' => TokenType::PLAIN_SCALAR,
+        'text' => 'value3',
+    ],
+    [
+        'type' => TokenType::NEWLINE,
+        'text' => "\n",
+    ],
+];
