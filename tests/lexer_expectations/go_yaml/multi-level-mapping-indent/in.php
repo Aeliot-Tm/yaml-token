39b8f5ce8898a02be538a289e82f08f3795c96ad
Anatoliy Melnikov <5785276@gmail.com>
2026-05-03 11:06:25 +0300

    Covering testing

---
+++ tests/lexer_expectations/go_yaml/multi-level-mapping-indent/in.php
@@ -0,0 +1,120 @@
+<?php
+
+declare(strict_types=1);
+
+use Aeliot\YamlToken\Enum\TokenType;
+
+return [
+    [
+        'type' => TokenType::PLAIN_SCALAR,
+        'text' => 'a',
+    ],
+    [
+        'type' => TokenType::VALUE_INDICATOR,
+        'text' => ':',
+    ],
+    [
+        'type' => TokenType::NEWLINE,
+        'text' => "\n",
+    ],
+    [
+        'type' => TokenType::INDENTATION,
+        'text' => '  ',
+    ],
+    [
+        'type' => TokenType::PLAIN_SCALAR,
+        'text' => 'b',
+    ],
+    [
+        'type' => TokenType::VALUE_INDICATOR,
+        'text' => ':',
+    ],
+    [
+        'type' => TokenType::NEWLINE,
+        'text' => "\n",
+    ],
+    [
+        'type' => TokenType::INDENTATION,
+        'text' => '    ',
+    ],
+    [
+        'type' => TokenType::PLAIN_SCALAR,
+        'text' => 'c',
+    ],
+    [
+        'type' => TokenType::VALUE_INDICATOR,
+        'text' => ':',
+    ],
+    [
+        'type' => TokenType::WHITESPACE,
+        'text' => ' ',
+    ],
+    [
+        'type' => TokenType::PLAIN_SCALAR,
+        'text' => 'd',
+    ],
+    [
+        'type' => TokenType::NEWLINE,
+        'text' => "\n",
+    ],
+    [
+        'type' => TokenType::INDENTATION,
+        'text' => '  ',
+    ],
+    [
+        'type' => TokenType::PLAIN_SCALAR,
+        'text' => 'e',
+    ],
+    [
+        'type' => TokenType::VALUE_INDICATOR,
+        'text' => ':',
+    ],
+    [
+        'type' => TokenType::NEWLINE,
+        'text' => "\n",
+    ],
+    [
+        'type' => TokenType::INDENTATION,
+        'text' => '    ',
+    ],
+    [
+        'type' => TokenType::PLAIN_SCALAR,
+        'text' => 'f',
+    ],
+    [
+        'type' => TokenType::VALUE_INDICATOR,
+        'text' => ':',
+    ],
+    [
+        'type' => TokenType::WHITESPACE,
+        'text' => ' ',
+    ],
+    [
+        'type' => TokenType::PLAIN_SCALAR,
+        'text' => 'g',
+    ],
+    [
+        'type' => TokenType::NEWLINE,
+        'text' => "\n",
+    ],
+    [
+        'type' => TokenType::PLAIN_SCALAR,
+        'text' => 'h',
+    ],
+    [
+        'type' => TokenType::VALUE_INDICATOR,
+        'text' => ':',
+    ],
+    [
+        'type' => TokenType::WHITESPACE,
+        'text' => ' ',
+    ],
+    [
+        'type' => TokenType::PLAIN_SCALAR,
+        'text' => 'i',
+    ],
+    [
+        'type' => TokenType::NEWLINE,
+        'text' => "\n",
+    ],
+];
---
+++ tests/lexer_expectations/go_yaml/multiline-doublequoted-flow-mapping-key-without-value/in.php
@@ -0,0 +1,120 @@
+<?php
+
+declare(strict_types=1);
+
+use Aeliot\YamlToken\Enum\TokenType;
+
+return [
+    [
+        'type' => TokenType::DOCUMENT_START,
+        'text' => '---',
+    ],
+    [
+        'type' => TokenType::NEWLINE,
+        'text' => "\n",
+    ],
+    [
+        'type' => TokenType::SEQUENCE_ENTRY,
+        'text' => '-',
+    ],
+    [
+        'type' => TokenType::WHITESPACE,
+        'text' => ' ',
+    ],
+    [
+        'type' => TokenType::FLOW_MAPPING_START,
+        'text' => '{',
+    ],
+    [
+        'type' => TokenType::WHITESPACE,
+        'text' => ' ',
+    ],
+    [
+        'type' => TokenType::DOUBLE_QUOTED_SCALAR,
+        'text' => '"single line"',
+    ],
+    [
+        'type' => TokenType::FLOW_ENTRY,
+        'text' => ',',
+    ],
+    [
+        'type' => TokenType::WHITESPACE,
+        'text' => ' ',
+    ],
+    [
+        'type' => TokenType::PLAIN_SCALAR,
+        'text' => 'a',
+    ],
+    [
+        'type' => TokenType::VALUE_INDICATOR,
+        'text' => ':',
+    ],
+    [
+        'type' => TokenType::WHITESPACE,
+        'text' => ' ',
+    ],
+    [
+        'type' => TokenType::PLAIN_SCALAR,
+        'text' => 'b',
+    ],
+    [
+        'type' => TokenType::FLOW_MAPPING_END,
+        'text' => '}',
+    ],
+    [
+        'type' => TokenType::NEWLINE,
+        'text' => "\n",
+    ],
+    [
+        'type' => TokenType::SEQUENCE_ENTRY,
+        'text' => '-',
+    ],
+    [
+        'type' => TokenType::WHITESPACE,
+        'text' => ' ',
+    ],
+    [
+        'type' => TokenType::FLOW_MAPPING_START,
+        'text' => '{',
+    ],
+    [
+        'type' => TokenType::WHITESPACE,
+        'text' => ' ',
+    ],
+    [
+        'type' => TokenType::DOUBLE_QUOTED_SCALAR,
+        'text' => "\"multi\n  line\"",
+    ],
+    [
+        'type' => TokenType::FLOW_ENTRY,
+        'text' => ',',
+    ],
+    [
+        'type' => TokenType::WHITESPACE,
+        'text' => ' ',
+    ],
+    [
+        'type' => TokenType::PLAIN_SCALAR,
+        'text' => 'a',
+    ],
+    [
+        'type' => TokenType::VALUE_INDICATOR,
+        'text' => ':',
+    ],
+    [
+        'type' => TokenType::WHITESPACE,
+        'text' => ' ',
+    ],
+    [
+        'type' => TokenType::PLAIN_SCALAR,
+        'text' => 'b',
+    ],
+    [
+        'type' => TokenType::FLOW_MAPPING_END,
+        'text' => '}',
+    ],
+    [
+        'type' => TokenType::NEWLINE,
+        'text' => "\n",
+    ],
+];
