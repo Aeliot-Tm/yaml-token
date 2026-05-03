27f6e8e26d5667634c1c93da07a2b14561d6177c
Anatoliy Melnikov <5785276@gmail.com>
2026-05-03 10:22:57 +0300

    Covering testing

---
+++ tests/lexer_expectations/go_yaml/mixed-block-mapping-explicit-to-implicit/in.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+use Aeliot\YamlToken\Enum\TokenType;
+
+return [
+    [
+        'type' => TokenType::EXPLICIT_KEY_INDICATOR,
+        'text' => '?',
+    ],
+    [
+        'type' => TokenType::WHITESPACE,
+        'text' => ' ',
+    ],
+    [
+        'type' => TokenType::PLAIN_SCALAR,
+        'text' => 'a',
+    ],
+    [
+        'type' => TokenType::NEWLINE,
+        'text' => "\n",
+    ],
+    [
+        'type' => TokenType::VALUE_INDICATOR,
+        'text' => ':',
+    ],
+    [
+        'type' => TokenType::WHITESPACE,
+        'text' => ' ',
+    ],
+    [
+        'type' => TokenType::PLAIN_SCALAR,
+        'text' => '1.3',
+    ],
+    [
+        'type' => TokenType::NEWLINE,
+        'text' => "\n",
+    ],
+    [
+        'type' => TokenType::PLAIN_SCALAR,
+        'text' => 'fifteen',
+    ],
+    [
+        'type' => TokenType::VALUE_INDICATOR,
+        'text' => ':',
+    ],
+    [
+        'type' => TokenType::WHITESPACE,
+        'text' => ' ',
+    ],
+    [
+        'type' => TokenType::PLAIN_SCALAR,
+        'text' => 'd',
+    ],
+    [
+        'type' => TokenType::NEWLINE,
+        'text' => "\n",
+    ],
+];
---
+++ tests/lexer_expectations/go_yaml/mixed-block-mapping-implicit-to-explicit/in.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+use Aeliot\YamlToken\Enum\TokenType;
+
+return [
+    [
+        'type' => TokenType::PLAIN_SCALAR,
+        'text' => 'a',
+    ],
+    [
+        'type' => TokenType::VALUE_INDICATOR,
+        'text' => ':',
+    ],
+    [
+        'type' => TokenType::WHITESPACE,
+        'text' => ' ',
+    ],
+    [
+        'type' => TokenType::PLAIN_SCALAR,
+        'text' => '4.2',
+    ],
+    [
+        'type' => TokenType::NEWLINE,
+        'text' => "\n",
+    ],
+    [
+        'type' => TokenType::EXPLICIT_KEY_INDICATOR,
+        'text' => '?',
+    ],
+    [
+        'type' => TokenType::WHITESPACE,
+        'text' => ' ',
+    ],
+    [
+        'type' => TokenType::PLAIN_SCALAR,
+        'text' => 'd',
+    ],
+    [
+        'type' => TokenType::NEWLINE,
+        'text' => "\n",
+    ],
+    [
+        'type' => TokenType::VALUE_INDICATOR,
+        'text' => ':',
+    ],
+    [
+        'type' => TokenType::WHITESPACE,
+        'text' => ' ',
+    ],
+    [
+        'type' => TokenType::PLAIN_SCALAR,
+        'text' => '23',
+    ],
+    [
+        'type' => TokenType::NEWLINE,
+        'text' => "\n",
+    ],
+];
