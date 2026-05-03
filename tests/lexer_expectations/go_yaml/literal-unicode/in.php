bad76ff5f30f5297d05b6401c7a1e8eb0a47a0f8
Anatoliy Melnikov <5785276@gmail.com>
2026-05-03 10:12:28 +0300

    Covering testing

---
+++ tests/lexer_expectations/go_yaml/literal-unicode/in.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+use Aeliot\YamlToken\Enum\TokenType;
+
+return [
+    [
+        'type' => TokenType::DOCUMENT_START,
+        'text' => '---',
+    ],
+    [
+        'type' => TokenType::NEWLINE,
+        'text' => "\n",
+    ],
+    [
+        'type' => TokenType::PLAIN_SCALAR,
+        'text' => 'wanted',
+    ],
+    [
+        'type' => TokenType::VALUE_INDICATOR,
+        'text' => ':',
+    ],
+    [
+        'type' => TokenType::WHITESPACE,
+        'text' => ' ',
+    ],
+    [
+        'type' => TokenType::PLAIN_SCALAR,
+        'text' => 'love ♥ and peace ☮',
+    ],
+    [
+        'type' => TokenType::NEWLINE,
+        'text' => "\n",
+    ],
+];
