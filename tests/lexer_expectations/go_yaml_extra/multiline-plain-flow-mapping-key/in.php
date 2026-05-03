1af449074c32bd9680c64af7dd88dfddd243d245
Anatoliy Melnikov <5785276@gmail.com>
2026-05-03 12:28:02 +0300

    Covering testing

---
+++ tests/lexer_expectations/go_yaml_extra/multiline-plain-flow-mapping-key/in.php
@@ -0,0 +1,108 @@
+<?php
+
+declare(strict_types=1);
+
+use Aeliot\YamlToken\Enum\TokenType;
+
+return [
+    [
+        'type' => TokenType::DOCUMENT_START,
+        'text' => '---',
+    ],
+    [
+        'type' => TokenType::NEWLINE,
+        'text' => "\n",
+    ],
+    [
+        'type' => TokenType::SEQUENCE_ENTRY,
+        'text' => '-',
+    ],
+    [
+        'type' => TokenType::WHITESPACE,
+        'text' => ' ',
+    ],
+    [
+        'type' => TokenType::FLOW_MAPPING_START,
+        'text' => '{',
+    ],
+    [
+        'type' => TokenType::WHITESPACE,
+        'text' => ' ',
+    ],
+    [
+        'type' => TokenType::PLAIN_SCALAR,
+        'text' => 'single line',
+    ],
+    [
+        'type' => TokenType::VALUE_INDICATOR,
+        'text' => ':',
+    ],
+    [
+        'type' => TokenType::WHITESPACE,
+        'text' => ' ',
+    ],
+    [
+        'type' => TokenType::PLAIN_SCALAR,
+        'text' => 'value',
+    ],
+    [
+        'type' => TokenType::FLOW_MAPPING_END,
+        'text' => '}',
+    ],
+    [
+        'type' => TokenType::NEWLINE,
+        'text' => "\n",
+    ],
+    [
+        'type' => TokenType::SEQUENCE_ENTRY,
+        'text' => '-',
+    ],
+    [
+        'type' => TokenType::WHITESPACE,
+        'text' => ' ',
+    ],
+    [
+        'type' => TokenType::FLOW_MAPPING_START,
+        'text' => '{',
+    ],
+    [
+        'type' => TokenType::WHITESPACE,
+        'text' => ' ',
+    ],
+    [
+        'type' => TokenType::PLAIN_SCALAR,
+        'text' => 'multi',
+    ],
+    [
+        'type' => TokenType::NEWLINE,
+        'text' => "\n",
+    ],
+    [
+        'type' => TokenType::WHITESPACE,
+        'text' => '  ',
+    ],
+    [
+        'type' => TokenType::PLAIN_SCALAR,
+        'text' => 'line',
+    ],
+    [
+        'type' => TokenType::VALUE_INDICATOR,
+        'text' => ':',
+    ],
+    [
+        'type' => TokenType::WHITESPACE,
+        'text' => ' ',
+    ],
+    [
+        'type' => TokenType::PLAIN_SCALAR,
+        'text' => 'value',
+    ],
+    [
+        'type' => TokenType::FLOW_MAPPING_END,
+        'text' => '}',
+    ],
+    [
+        'type' => TokenType::NEWLINE,
+        'text' => "\n",
+    ],
+];
