efc32af33851af4fe27de3c8006c129c432c9834
Anatoliy Melnikov <5785276@gmail.com>
2026-05-24 22:24:53 +0300

    Covering testing

---
+++ tests/lexer_expectations/go_yaml/various-trailing-comments/in.php
@@ -0,0 +1,292 @@
+<?php
+
+declare(strict_types=1);
+
+use Aeliot\YamlToken\Enum\TokenType;
+
+return [
+    [
+        'type' => TokenType::PLAIN_SCALAR,
+        'text' => 'a',
+    ],
+    [
+        'type' => TokenType::VALUE_INDICATOR,
+        'text' => ':',
+    ],
+    [
+        'type' => TokenType::WHITESPACE,
+        'text' => ' ',
+    ],
+    [
+        'type' => TokenType::DOUBLE_QUOTED_SCALAR,
+        'text' => "\"double\n  quotes\"",
+    ],
+    [
+        'type' => TokenType::WHITESPACE,
+        'text' => ' ',
+    ],
+    [
+        'type' => TokenType::COMMENT,
+        'text' => '# lala',
+    ],
+    [
+        'type' => TokenType::NEWLINE,
+        'text' => "\n",
+    ],
+    [
+        'type' => TokenType::PLAIN_SCALAR,
+        'text' => 'b',
+    ],
+    [
+        'type' => TokenType::VALUE_INDICATOR,
+        'text' => ':',
+    ],
+    [
+        'type' => TokenType::WHITESPACE,
+        'text' => ' ',
+    ],
+    [
+        'type' => TokenType::PLAIN_SCALAR,
+        'text' => 'plain',
+    ],
+    [
+        'type' => TokenType::NEWLINE,
+        'text' => "\n",
+    ],
+    [
+        'type' => TokenType::INDENTATION,
+        'text' => ' ',
+    ],
+    [
+        'type' => TokenType::PLAIN_SCALAR,
+        'text' => 'value',
+    ],
+    [
+        'type' => TokenType::WHITESPACE,
+        'text' => '  ',
+    ],
+    [
+        'type' => TokenType::COMMENT,
+        'text' => '# lala',
+    ],
+    [
+        'type' => TokenType::NEWLINE,
+        'text' => "\n",
+    ],
+    [
+        'type' => TokenType::PLAIN_SCALAR,
+        'text' => 'c',
+    ],
+    [
+        'type' => TokenType::WHITESPACE,
+        'text' => '  ',
+    ],
+    [
+        'type' => TokenType::VALUE_INDICATOR,
+        'text' => ':',
+    ],
+    [
+        'type' => TokenType::WHITESPACE,
+        'text' => ' ',
+    ],
+    [
+        'type' => TokenType::COMMENT,
+        'text' => '#lala',
+    ],
+    [
+        'type' => TokenType::NEWLINE,
+        'text' => "\n",
+    ],
+    [
+        'type' => TokenType::INDENTATION,
+        'text' => '  ',
+    ],
+    [
+        'type' => TokenType::PLAIN_SCALAR,
+        'text' => 'd',
+    ],
+    [
+        'type' => TokenType::NEWLINE,
+        'text' => "\n",
+    ],
+    [
+        'type' => TokenType::EXPLICIT_KEY_INDICATOR,
+        'text' => '?',
+    ],
+    [
+        'type' => TokenType::WHITESPACE,
+        'text' => ' ',
+    ],
+    [
+        'type' => TokenType::COMMENT,
+        'text' => '# lala',
+    ],
+    [
+        'type' => TokenType::NEWLINE,
+        'text' => "\n",
+    ],
+    [
+        'type' => TokenType::INDENTATION,
+        'text' => ' ',
+    ],
+    [
+        'type' => TokenType::SEQUENCE_ENTRY,
+        'text' => '-',
+    ],
+    [
+        'type' => TokenType::WHITESPACE,
+        'text' => ' ',
+    ],
+    [
+        'type' => TokenType::PLAIN_SCALAR,
+        'text' => 'seq1',
+    ],
+    [
+        'type' => TokenType::NEWLINE,
+        'text' => "\n",
+    ],
+    [
+        'type' => TokenType::VALUE_INDICATOR,
+        'text' => ':',
+    ],
+    [
+        'type' => TokenType::WHITESPACE,
+        'text' => ' ',
+    ],
+    [
+        'type' => TokenType::COMMENT,
+        'text' => '# lala',
+    ],
+    [
+        'type' => TokenType::NEWLINE,
+        'text' => "\n",
+    ],
+    [
+        'type' => TokenType::INDENTATION,
+        'text' => ' ',
+    ],
+    [
+        'type' => TokenType::SEQUENCE_ENTRY,
+        'text' => '-',
+    ],
+    [
+        'type' => TokenType::WHITESPACE,
+        'text' => ' ',
+    ],
+    [
+        'type' => TokenType::COMMENT,
+        'text' => '#lala',
+    ],
+    [
+        'type' => TokenType::NEWLINE,
+        'text' => "\n",
+    ],
+    [
+        'type' => TokenType::INDENTATION,
+        'text' => '  ',
+    ],
+    [
+        'type' => TokenType::PLAIN_SCALAR,
+        'text' => 'seq2',
+    ],
+    [
+        'type' => TokenType::NEWLINE,
+        'text' => "\n",
+    ],
+    [
+        'type' => TokenType::PLAIN_SCALAR,
+        'text' => 'e',
+    ],
+    [
+        'type' => TokenType::VALUE_INDICATOR,
+        'text' => ':',
+    ],
+    [
+        'type' => TokenType::NEWLINE,
+        'text' => "\n",
+    ],
+    [
+        'type' => TokenType::INDENTATION,
+        'text' => ' ',
+    ],
+    [
+        'type' => TokenType::ANCHOR,
+        'text' => '&node',
+    ],
+    [
+        'type' => TokenType::WHITESPACE,
+        'text' => ' ',
+    ],
+    [
+        'type' => TokenType::COMMENT,
+        'text' => '# lala',
+    ],
+    [
+        'type' => TokenType::NEWLINE,
+        'text' => "\n",
+    ],
+    [
+        'type' => TokenType::INDENTATION,
+        'text' => ' ',
+    ],
+    [
+        'type' => TokenType::SEQUENCE_ENTRY,
+        'text' => '-',
+    ],
+    [
+        'type' => TokenType::WHITESPACE,
+        'text' => ' ',
+    ],
+    [
+        'type' => TokenType::PLAIN_SCALAR,
+        'text' => 'x',
+    ],
+    [
+        'type' => TokenType::VALUE_INDICATOR,
+        'text' => ':',
+    ],
+    [
+        'type' => TokenType::WHITESPACE,
+        'text' => ' ',
+    ],
+    [
+        'type' => TokenType::PLAIN_SCALAR,
+        'text' => 'y',
+    ],
+    [
+        'type' => TokenType::NEWLINE,
+        'text' => "\n",
+    ],
+    [
+        'type' => TokenType::PLAIN_SCALAR,
+        'text' => 'block',
+    ],
+    [
+        'type' => TokenType::VALUE_INDICATOR,
+        'text' => ':',
+    ],
+    [
+        'type' => TokenType::WHITESPACE,
+        'text' => ' ',
+    ],
+    [
+        'type' => TokenType::FOLDED_BLOCK_SCALAR_INDICATOR,
+        'text' => '>',
+    ],
+    [
+        'type' => TokenType::WHITESPACE,
+        'text' => ' ',
+    ],
+    [
+        'type' => TokenType::COMMENT,
+        'text' => '# lala',
+    ],
+    [
+        'type' => TokenType::NEWLINE,
+        'text' => "\n",
+    ],
+    [
+        'type' => TokenType::FOLDED_BLOCK_SCALAR,
+        'text' => "  abcde\n",
+    ],
+];
